d1437b44c079680a358ac82a43554b57649cf3b6
Grenze <hit_grenze_plus@163.com>
2018-03-21 10:02:47 +0800

    v1.1

--- src/Main.java
+++ src/Main.java
@@ -277,12 +277,16 @@ public class Main {
         testpcj();//chain create directories or files
         //testFileUtils();
         //testDefaultFileSystemAbstraction();
+        //testJavaNormal();
 
 
 
 
 
 
+    }
+
+    public static void testJavaNormal(){
 
     }
 
@@ -456,11 +460,15 @@ public class Main {
         *
         * */
         Engineer eng0 = new Engineer(0,"ghost","umbrella","clear");
-        Engineer eng1 = new Engineer(1,"heaven","sun","fill");
+        //Engineer eng1 = new Engineer(1,"heaven","sun","fill");
         ObjectDirectory.put("1",eng0);
-        ObjectDirectory.put("1",eng1);
-        Engineer eng2 = ObjectDirectory.get("1",Engineer.class);
+        ObjectDirectory.put("2",eng0);
+
+        ObjectDirectory.remove("1",Engineer.class);
+        Engineer eng2 = ObjectDirectory.get("2",Engineer.class);
         System.out.println(eng2.getName());
+        //System.out.println(ObjectDirectory.get("123",Engineer.class)==null);//true
+
 
         //eng0.createGroup("littlebuster").createGroup("tinybuster");
         //Engineer eng1 = (Engineer) ObjectDirectory.get("tinybuster",PersistentHashMap.class).get(persistent("lin"));
