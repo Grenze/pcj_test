73549e1f92112d54127866c15594df053c2989f8
Grenze <hit_grenze_plus@163.com>
2018-03-22 20:20:35 +0800

    v1.1

--- src/org/neo4j/io/nvmfs/NvmFilDir.java
+++ src/org/neo4j/io/nvmfs/NvmFilDir.java
@@ -1,6 +1,5 @@
 package org.neo4j.io.nvmfs;
 
-import lib.util.persistent.ObjectDirectory;
 import lib.util.persistent.ObjectPointer;
 import lib.util.persistent.PersistentObject;
 import lib.util.persistent.types.BooleanField;
@@ -8,7 +7,6 @@ import lib.util.persistent.types.ObjectType;
 import lib.util.persistent.types.StringField;
 
 import java.io.File;
-import java.io.IOException;
 
 import static lib.util.persistent.Util.persistent;
 
@@ -20,38 +18,43 @@ public class NvmFilDir  extends PersistentObject{
 
     private static final ObjectType<NvmFilDir> TYPE = ObjectType.withFields(NvmFilDir.class, LOCALINDEX, FILECONTENT, ISFILE, ISDIRECTORY);
 
-    public NvmFilDir(File file, boolean isFile, boolean isDirectory) throws IOException{
+    public NvmFilDir(File file, boolean isFile, boolean isDirectory) {
         super(TYPE);
-        setLocalIndex();
-        setFileContent();
+        setLocalIndex("");
+        setFileContent("");
         setIsFile(isFile);
         setIsDirectory(isDirectory);
-        ObjectDirectory.put(file.getCanonicalPath(),this);
     }
 
     private NvmFilDir(ObjectPointer<NvmFilDir> p){
         super(p);
     }
+    //copy
+    public NvmFilDir(NvmFilDir nvmFilDir){
+        super(TYPE);
+        setLocalIndex(nvmFilDir.getLocalIndex());
+        setFileContent(nvmFilDir.getFileContent());
+        setIsFile(nvmFilDir.getIsFile());
+        setIsDirectory(nvmFilDir.getIsDirectory());
+    }
 
-    //private NvmFilDir(NvmFilDir nvmFilDir){
-
-    //}
-
-    private void setLocalIndex(){
-        setObjectField(LOCALINDEX, persistent(""));
+    private void setLocalIndex(String localIndex){
+        setObjectField(LOCALINDEX, persistent(localIndex));
     }
 
     private String getLocalIndex(){
         return getObjectField(LOCALINDEX).toString();
     }
 
-    private void setFileContent(){
-        setObjectField(FILECONTENT, persistent(""));
+
+    private void setFileContent(String fileContent){
+        setObjectField(FILECONTENT, persistent(fileContent));
     }
 
     private String getFileContent(){
         return getObjectField(FILECONTENT).toString();
     }
+
 
     private void setIsFile(boolean isFile){
         setBooleanField(ISFILE, isFile);
@@ -60,6 +63,7 @@ public class NvmFilDir  extends PersistentObject{
     private boolean getIsFile(){
         return getBooleanField(ISFILE);
     }
+
 
     private void setIsDirectory(boolean isDirectory){
         setBooleanField(ISDIRECTORY, isDirectory);
