57a7608c0e81ad71bb6e6ea229c1387b088ab022
Grenze <hit_grenze_plus@163.com>
2018-03-24 18:25:53 +0800

    v1.1

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,22 @@ public class Main {
     public static void main(String[] agrs) throws IOException {
 
         //Utiltest.testAll();
-        NvmFileUtils.writeToFile(new File("file2"), "Violate 123456789", false);
-        NvmFileUtils.truncateFile(new File("file2"), 10);
-        NvmFileUtils.moveFile(new File("file2"), new File("MFile/M1/file2"));
-        //NvmFileUtils.moveFileToDirectory(new File("file2"), new File("MFile/M1/"));
-        NvmFileUtils.printDirectory();
+        NvmFileUtils.writeToFile(new File("file1"), "Violate 123456789", false);
+        NvmFileUtils.truncateFile(new File("file1"), 15);
+        //NvmFileUtils.moveFile(new File("file2"), new File("MFile/M1/file2"));
+        NvmFileUtils.copyFile(new File("file1"), new File("MFile/X1/file1"));
+        NvmFileUtils.moveFileToDirectory(new File("file1"), new File("MFile/M1"));
+        NvmFileUtils.copyRecursively(new File("MFile/X1"), new File("MFile/M1"));
+
         //System.out.println(NvmFilDir.removeNvmFilDir(new File("MFile/M1/file2")).getSize());
 
-        NvmFileUtils.renameFile(new File("MFile/M1/"), new File("MFile/X1"));
+        NvmFileUtils.renameFile(new File("MFile/M1/"), new File("MFile/F1"));
         NvmDefaultFileSystemAbstraction ndfs = new NvmDefaultFileSystemAbstraction();
         printFiles(ndfs.listFiles(new File("MFile")));
         NvmFileUtils.printDirectory();
         //System.out.println(new File("../../../../..").getCanonicalFile().getParentFile());
         //System.out.println(NvmFilDir.exists(new File("../../../../")));
-        System.out.println(NvmFileUtils.readTextFile(new File("MFile/X1/file2"), Charset.forName("utf8")));
+        System.out.println(NvmFileUtils.readTextFile(new File("MFile/F1/file1"), Charset.forName("utf8")));
         System.out.print("----bottom line----");
 
 
